82c4a681a76795f98af0ae643ca0b1fc4a4bfca9
lpferrari2204 <lpferrari2204@gmail.com>
2012-02-03 21:51:40 +0000

    new release

---
+++ jogocarta/src/truco/Truco.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package truco;
+
+/**
+ *
+ * @author Magaly
+ */
+public class Truco {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Teste t = new Teste();
+        t.setVisible(true);
+    }
+}
